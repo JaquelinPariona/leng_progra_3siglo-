181d0712d0b6f6aa28d528413cfa98fe8e7bb306
JaquelinPariona <juradojaqueline85@mail.com>
2026-05-22 10:17:44 -0500

    clases de boleanos y practica

---
+++ 03.tipos_datos/08.boleanos.py
@@ -0,0 +1,10 @@
+asistencia:bool=True # si asistio
+is_active=bool=False
+# Cuando la variable de declarar por bprimera vez se tiene que indicar el tipo de dato
+asistencia=1
+is_active=0
+# Cuando  reutilizamos la variable ya no es necesario indicar el tipo de dato.
+print (asistencia)
+
+# Mutabilidad de datos 
+## Se refiere que en python le podemos cambiar de valor a las variables 
---
+++ 03.tipos_datos/practica_tipos_datos.py
@@ -0,0 +1,23 @@
+# Crear haciendo uso de las clases anteriores, una calculadora.
+# Que pida al usuario dos numeros enteros y luego
+## De que manera ordenada mostrar por terminal el resultado
+
+
+mensaje_bienvenido:str="""
+======================================
+=     BIENVENIDO A MI CALCULADORA    =
+======================================
+"""
+print(mensaje_bienvenido)
+print("A continuacion ingrese dos numeros para realizar la suma")
+numero_uno:int=int(input("Ingrese el primer numero:"))
+numero_dos:int=int(input("Ingrese el segundo numero:"))
+resultado_suma:int=numero_uno+numero_dos
+mensaje_resultado:str=f"""
+el resultado de la suma del numero
+{numero_uno}
+con el numero 
+{numero_dos}
+es igual a {resultado_suma} 
+"""
+print(mensaje_resultado)
